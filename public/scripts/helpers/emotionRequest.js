de5746eba384b147ff9f2d3293b5a250945f78ad
Robert Jones <robert.g.jones@gmail.com>
2017-09-14 18:12:04 +0100

    Update Emotion Request

--- public/scripts/helpers/emotionRequest.js
+++ public/scripts/helpers/emotionRequest.js
@@ -1,61 +1,65 @@
 (function(exports) {
 
-
   function APIRequest(blob, picture) {
     $.ajax({
         url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?",
-        beforeSend: function(xhrObj){
-          xhrObj.setRequestHeader("Content-Type",'application/octet-stream');
-          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key",'e01d4e40138540cfaa19b379aeded881');
+        beforeSend: function(xhrObj) {
+          xhrObj.setRequestHeader("Content-Type", 'application/octet-stream');
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", 'e01d4e40138540cfaa19b379aeded881');
         },
         type: 'POST',
         processData: false,
         contentType: 'application/octet-stream',
         data: blob,
-     })
-    .done(function(data) {
-      setResults(picture, data);
-    })
-    // this data is the info we need!!!
-    .fail(function() {alert("error");});
+      })
+      .done(function(data) {
+        setResults(picture, data);
+      })
+      .fail(function() {
+        alert("error");
+      });
   }
 
 
-          function makeBlob (dataURL) {
-              var BASE64_MARKER = ';base64,';
-              if (dataURL.indexOf(BASE64_MARKER) == -1) {
-                  var parts = dataURL.split(',');
-                  var contentType = parts[0].split(':')[1];
-                  var raw = decodeURIComponent(parts[1]);
-                  return new Blob([raw], { type: contentType });
-              }
-              var parts = dataURL.split(BASE64_MARKER);
-              var contentType = parts[0].split(':')[1];
-              var raw = window.atob(parts[1]);
-              var rawLength = raw.length;
+  function makeBlob(dataURL) {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+      var parts = dataURL.split(',');
+      var contentType = parts[0].split(':')[1];
+      var raw = decodeURIComponent(parts[1]);
+      return new Blob([raw], {
+        type: contentType
+      });
+    }
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
 
-              var uInt8Array = new Uint8Array(rawLength);
+    var uInt8Array = new Uint8Array(rawLength);
 
-              for (var i = 0; i < rawLength; ++i) {
-                  uInt8Array[i] = raw.charCodeAt(i);
-              }
+    for (var i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
 
-              return new Blob([uInt8Array], { type: contentType });
-          }
+    return new Blob([uInt8Array], {
+      type: contentType
+    });
+  }
 
-          function setResults(picture, data) {
-            picture.happiness = data[0].scores.happiness;
-            picture.sadness = data[0].scores.sadness;
-            picture.surprise = data[0].scores.surprise;
-            picture.disgust = data[0].scores.disgust;
-            picture.contempt = data[0].scores.contempt;
-            picture.fear = data[0].scores.fear;
-            picture.neutral = data[0].scores.neutral;
-            picture.anger = data[0].scores.anger;
-          }
+  function setResults(picture, data) {
+    picture.happiness = data[0].scores.happiness;
+    picture.sadness = data[0].scores.sadness;
+    picture.surprise = data[0].scores.surprise;
+    picture.disgust = data[0].scores.disgust;
+    picture.contempt = data[0].scores.contempt;
+    picture.fear = data[0].scores.fear;
+    picture.neutral = data[0].scores.neutral;
+    picture.anger = data[0].scores.anger;
+  }
 
 
-    exports.APIRequest = APIRequest;
-    exports.makeBlob = makeBlob;
+  exports.APIRequest = APIRequest;
+  exports.makeBlob = makeBlob;
 
 })(this);
--- test/emotionRequest_spec.js
+++ test/emotionRequest_spec.js
@@ -1,20 +1,23 @@
-// const apiRequest = require('../public/scripts/helpers/emotionRequest.js')
-// const chai = require('chai');
-// const sinon = require('sinon');
-// const expect = chai.expect;
-//
-// describe('Emotion API request', function() {
-//
-//   describe('the APIRequest function', function() {
-//
-//     after(function() {
-//       jQuery.ajax.restore();
-//     })
-//
-//     it('should l;aksdnfkandsf', function() {
-//       sinon.stub(jQuery, 'ajax');
-//       APIRequest(blob, picture);
-//       expect();
-//     });
-//   });
-// })
+/* Prototypes */
+const APIRequest = require('../public/scripts/helpers/emotionRequest.js')
+
+/* Test Libraries */
+const chai = require('chai');
+var expect = chai.expect;
+const sinon = require('sinon');
+
+describe('Emotion API request', function() {
+
+  describe('the APIRequest function', function() {
+
+    after(function() {
+      jQuery.ajax.restore();
+    });
+
+    it('gives a picture attributes', function() {
+      var picture = new Picture();
+      sinon.stub(jQuery, 'ajax');
+      APIRequest(blob, picture);
+    });
+  });
+})
