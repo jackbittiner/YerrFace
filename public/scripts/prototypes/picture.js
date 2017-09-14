a0f265295f57ebedac6ada75bb94c679b22c44af
Jack Bittiner <jackbittiner@hotmail.com>
2017-09-14 18:08:38 +0100

    fixed undefined results display bug - hopefully

--- public/scripts/prototypes/picture.js
+++ public/scripts/prototypes/picture.js
@@ -2,6 +2,14 @@
 
   function Picture(data) {
     this.imageData = data;
+    this.happiness = null;
+    this.sadness = null;
+    this.surprise = null;
+    this.disgust = null;
+    this.contempt = null;
+    this.fear = null;
+    this.neutral = null;
+    this.anger = null;
   }
 
  exports.Picture = Picture;
